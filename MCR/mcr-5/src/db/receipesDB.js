9fd6fcd280650a373225b79340b81bd7314f2af2
Tushar Anekar <anekar.tushar@gmail.com>
2023-07-01 19:23:29 +0530

    feat- data added

---
+++ MCR/mcr-5/src/db/receipesDB.js
@@ -0,0 +1,26 @@
+export const receipesDB = [
+    {
+        id: 1,
+        imageUrl: "https://pixabay.com/get/g3b12eec4013cf86ac2e7d68df31fb1530b6e81b587e876703fc4dc2bd9118727bac7e68fce891c19ddb1f5133a996c1486ae27149f2b2029303a9a452bb8c637_1280.jpg",
+        recipe_name: "Biryani",
+        ingredients: "Rice, water, spices, chicken",
+        cooking_instructions: ["Google it", "Follow the steps"],
+        cuisine_type: "Indian"
+    },
+    {
+        id: 2,
+        imageUrl: "https://pixabay.com/get/g0e0ea23fb74c8b2d0da2dbcc5b50618e1aad55ef5768c1b0bd88d5a62bb67f0c54c953b881d832661a9005641173eec7e462b58dd5252206fdcfc05f943410ee_1280.jpg",
+        recipe_name: "Burger",
+        ingredients: "Bread buns, veggies, chicken",
+        cooking_instructions: ["Google it", "Follow the steps"],
+        cuisine_type: "American"
+    },
+    {
+        id: 3,
+        imageUrl: "https://pixabay.com/get/ga15568899db9e6898715e969e4df1b860a0fb8f8284c68a2f77a90f58b5e5d3ebe46b6dd57645588f4a07b88c82d9bf1858a5b5ccf8e46a61dde57829c739929_1280.jpg",
+        recipe_name: "Pizza",
+        ingredients: "Bread base, veggies, chicken",
+        cooking_instructions: ["Google it", "Follow the steps"],
+        cuisine_type: "Italian"
+    }
+]
